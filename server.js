47d88b967c00dbc3d5de5ed4c05825c31a6ead62
Bhavya Bansal <bbansal@mbp-bbansal.local>
2018-10-13 18:22:48 -0400

    adding cors support

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ MongoClient.connect(MONGO_URL, (err, client) => {
   app.listen(8080, () => {
     console.log('listening on 8080')
 
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
+
     app.get('/', function(req, res) {
       res.send('Hello World')
     });
