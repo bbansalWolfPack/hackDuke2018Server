b789bbca6f1622077863ac889fd95f44ed33701c
Bhavya Bansal <bbansal@mbp-bbansal.local>
2018-10-13 17:36:49 -0400

    adding default route

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ var db
 MongoClient.connect(MONGO_URL, (err, client) => {
   if (err) return console.log(err)
   db = client.db('go-green') // whatever your database name is
-  app.listen(3000, () => {
-    console.log('listening on 3000')
+  app.listen(8080, () => {
+    console.log('listening on 8080')
+
+    app.get('/', function(req, res) {
+      res.send('Hello World')
+    });
 
     app.get('/userDetails', (req, res) => {
       // find user details from the database
