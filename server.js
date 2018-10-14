eca1850ee83da018f3ec6349932a9e5fbd6a3455
Bhavya Bansal <bbansal@mbp-bbansal.local>
2018-10-13 22:09:13 -0400

    adding put request

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var db
 MongoClient.connect(MONGO_URL, (err, client) => {
   if (err) return console.log(err)
   db = client.db('go-green') // whatever your database name is
-  app.listen(8080, () => {
+  app.listen(3000, () => {
     console.log('listening on 8080')
 
     app.use(function(req, res, next) {
@@ -21,7 +21,7 @@ MongoClient.connect(MONGO_URL, (err, client) => {
     });
 
     app.get('/userDetails', (req, res) => {
-      let email = req.params.userEmail;
+      let email = req.query.userEmail;
       // find user details from the database
       db.collection('User').findOne({"email": email}, function(err, document) {
         if (err) {
@@ -30,6 +30,48 @@ MongoClient.connect(MONGO_URL, (err, client) => {
           res.send(document);
         }
       });
+    });
+
+    app.put('/quotes', (req, res) => {
+      res.send('Hello World')
     })
+
+    // app.put('/updateUser', (req, res) => {
+    //   console.log("bhavya")
+    //   let userEmail = req.query.userEmail;
+    //   let newScore = req.query.newScore;
+    //   let isEBill = req.query.eBill;
+    //   if (isEBill) {
+    //     db.collections('quotes').findOneAndUpdate(
+    //       {
+    //         "email": userEmail
+    //       },
+    //       {
+    //         $inc: { "totalVists": 1, "eBillVisits": 1}
+    //       },
+    //       (err, result) => {
+    //         if (err) return res.send(err)
+    //         res.send(result)
+    //       }
+    //     )
+    //   } else {
+    //     db.collections('quotes').findOneAndUpdate(
+    //       {
+    //         "email": userEmail
+    //       },
+    //       {
+    //         $set: {
+    //           quote: req.body.quotes
+    //         },
+    //
+    //         $inc: { totalVists: 1}
+    //       },
+    //       (err, result) => {
+    //         if (err) return res.send(err)
+    //         res.send(result)
+    //       }
+    //     )
+    //   }
+    // })
   })
 })
